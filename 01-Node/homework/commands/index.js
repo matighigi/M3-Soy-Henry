b5ef0a5c1eb305eec2e906a7987f2e51d2b32d64
Matias Agustin Ghigi <matiasghigi@gmail.com>
2022-10-17 20:32:33 -0300

    Tarea 01 Realizada

--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,64 @@
+const fs = require('fs')
+const request = require('request')
 
 
+const echo = (args, print) => {
+    print(args.join(' '))
+}
+
+// const pwd = (args, print) => {
+//     print(__dirname)   ////muestra la ruta completa
+// }
+
+const pwd = (args, print) => {
+    print(__dirname.split('\\').at(-1)) //at (-1) toma el ultimo elemento del array
+}
+
+const date = (args, print) => {
+    print(Date())
+}
+
+const ls = (args, print) => {
+    fs.readdir('.',(err, files) => {
+        if(err) throw err
+        print(files.join('\n'))
+    })
+}
+
+const cat = (args, print) => {
+    fs.readFile(args.at(0), 'utf-8', (err, data) => {
+        if (err) throw err
+        print(data)
+    })
+}
+
+const head = (args, print) => {
+    fs.readFile(args.at(0), 'utf-8', (err, data) => {
+        if (err) throw err
+        print(data.split('\n').splice(0, 5).join('\n')) //agarra las 5 primeras lineas
+    })
+}
+
+const tail = (args, print) => {
+    fs.readFile(args.at(0), 'utf-8', (err, data) => {
+        if (err) throw err
+        print(data.split('\n').splice(-5).join('\n')) //agarra las 5 ultimas lineas
+    })
+}
+
+const curl = (args, print) => {
+    request(args[0], (err, data) => {
+        if(err) throw err
+        print(data.body)
+    }) 
+}
 module.exports = {
-
+    echo,
+    pwd,
+    date,
+    ls,
+    cat,
+    head,
+    tail,
+    curl
 }
