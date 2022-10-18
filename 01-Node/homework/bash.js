60fd5c94d5115c4f04f507bb2240d5fe33a64151
Matias Agustin Ghigi <matiasghigi@gmail.com>
2022-10-18 14:14:24 -0300

    Tarea 01 Realizada

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -14,3 +14,26 @@ process.stdin.on('data', (data) => {
     commands[cmd] ? commands[cmd](args, print) : print('command not found')
 
 })
+
+
+
+// const commands = require('./commands/index');
+
+// const print = (input) => {
+//     process.stdout.write(input + '\n')
+//     process.stdout.write('prompt > ')
+// }
+
+
+// process.stdout.write('prompt > ')
+
+// process.stdin.on('data', (data) => {
+//     let args = data.toString().trim().split(' ');
+//     // ['bash.js']
+
+//     let cmd = args.shift(); // 'cat'
+
+//     commands[cmd]
+//     ? commands[cmd](args, print)
+//     : print('Command invalid')
+// })
